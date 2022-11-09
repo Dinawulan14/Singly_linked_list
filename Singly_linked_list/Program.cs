da9f4746cc1b9dfca9dee3611911cce15f447c0f
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-09 18:39:02 +0700

    Membuat case 1, case 2, case 3, case 4

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -115,6 +115,81 @@ namespace Singly_linked_list
                 return false;
         }
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List obj = new List();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Menambah data kedalam list");
+                    Console.WriteLine("2. Menghapus data dari dalam list");
+                    Console.WriteLine("3. Melihat semua data didalam list");
+                    Console.WriteLine("4. mencari sebuah data didalam list");
+                    Console.WriteLine("5. Exit");
+                    Console.Write("\nMasukkan pilihan Anda (1-5): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.ListEmpty())
+                                {
+                                    Console.WriteLine("\nList Kosong");
+                                    break;
+                                }
+                                Console.Write("\nMasukkan nomor mahasiswa yang akan dihapus: ");
+                                int nim = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.delNode(nim) == false)
+                                    Console.WriteLine("\nData tidak ditemukan.");
+                                else
+                                    Console.WriteLine("Data dengan nomor mahasiswa " + nim + "dihapus ");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '4':
+                            {
+                                if (obj.ListEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList Kosong !");
+                                    break;
+                                }
+                                Node previous, current;
+                                previous = current = null;
+                                Console.Write("\nMasukkan nomor mahasiswa yang akan dicari: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref previous, ref current) == false)
+                                    Console.WriteLine("\nData tidak ditemukan.");
+                                else
+                                {
+                                    Console.WriteLine("\nData ketemu");
+                                    Console.WriteLine("\nNomor Mahasiswa: " + current.noMhs);
+                                    Console.WriteLine("\nNama: " + current.nama):
+                                }
+                                break;
+                            }
+                           
+                                }
+                            }
+
+                    }
+                }
+            }
+        }
+    }
 }
 
 
