70f35da8fe7d226c0a40643bbfb3844e153fcda7
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-09 16:59:23 +0700

    Menambahkan addNode

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Singly_linked_list
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
+        public int noMhs;
+        public string nama;
+        public Node next;
+    }
+    class List
+    {
+        Node START;
+        public List()
         {
+            START = null;
+        }
+
+        public void addNode()/*Method untuk menambahkan sebuah Node kedalam List*/
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+
         }
     }
 }
+
+        
+       
